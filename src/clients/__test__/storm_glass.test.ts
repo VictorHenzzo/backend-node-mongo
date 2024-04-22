3d7dc35ed92b83b7faa5f5aaba7223f5e9dd0b70
Victor <victorhenzzowp@gmail.com>
2024-04-22 18:04:37 -0300

    added stormglass client

---
+++ src/clients/__test__/storm_glass.test.ts
@@ -0,0 +1,21 @@
+import { StormGlass } from '@src/clients/storm_glass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalizedWeather3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = 29.672542;
+    const lng = 39.439769;
+
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalizedWeather3HoursFixture);
+  });
+});
